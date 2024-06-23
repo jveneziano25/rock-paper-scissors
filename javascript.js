e2fd853dbb3b9c6bb8d2cefa6ae1ae26f181d837
Josh Veneziano <jveneziano@proton.me>
2024-06-23 16:18:10 -0500

    Create function to prompt player to input their choice of rock, paper, or scissors. The input is normalized into all lowercase to accept any capitalization entered by user

--- javascript.js
+++ javascript.js
@@ -13,3 +13,17 @@ function getComputerChoice(choice){
         return choice;
     }
 }
+
+//Prompts player to enter choice and normalizes
+function getHumanChoice(choice){
+    let input = prompt('Enter Rock, Paper, or Scissors!');
+    //This normalizes the user input to make it all lowercase in case user inputs all caps
+    input = input.toLowerCase();
+    if(input == 'rock'){
+        return 'Rock';
+    } else if(input == 'paper'){
+        return 'Paper';
+    } else if (input == 'scissors'){
+        return 'Scissors';
+    }
+}
