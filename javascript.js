8ca241df3b68bd36d8ec327764568c8397f3a0af
Josh Veneziano <jveneziano@proton.me>
2024-06-30 11:35:50 -0500

    Add style to webpage to improve user experience.

--- javascript.js
+++ javascript.js
@@ -22,18 +22,45 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice){
     if(humanChoice === 'Paper' && computerChoice === 'Rock'){
         humanScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('Paper covers rock! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === 'Paper' && computerChoice === 'Scissors'){
         computerScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('Scissors cuts paper! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === 'Rock' && computerChoice === 'Paper'){
         computerScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('Paper covers rock! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === 'Rock' && computerChoice === 'Scissors'){
         humanScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = (' Rock smashes scissors! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === 'Scissors' && computerChoice === 'Paper'){
         humanScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('Scissors cuts paper! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === 'Scissors' && computerChoice === 'Rock'){
         computerScore++;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('Rock smashes scissors! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     } else if(humanChoice === computerChoice){
-        return;
+        let newDiv = document.createElement("div");
+        let container = document.getElementById("buttons");
+        newDiv.textContent = ('It\'s a tie! You: ' + humanScore + ' Computer: ' + computerScore + '.');
+        container.appendChild(newDiv);
     }
 }
 
@@ -59,28 +86,30 @@ function chooseScissors(){
 }
 
 function checkWinner(humanScore, computerScore){
-    if(!(humanScore > 2 || computerScore > 2)) {
-        if(humanScore === 2) {
+    if(!(humanScore > 5 || computerScore > 5)) {
+        if(humanScore === 5) {
             let newDiv = document.createElement("div");
+            newDiv.classList.add("winner");
             let container = document.getElementById("buttons");
-            newDiv.textContent = 'You\'ve reached 2 points! You win!';
+            newDiv.textContent = 'You\'ve reached 5 points! You win!';
             container.appendChild(newDiv);
+
             rockBtn.removeEventListener("click", chooseRock);
             paperBtn.removeEventListener("click", choosePaper);
             scissorsBtn.removeEventListener("click", chooseScissors);
-        }else if (computerScore === 2){
+
+        }else if (computerScore === 5){
             let newDiv = document.createElement("div");
+            newDiv.classList.add("winner");
             let container = document.getElementById("buttons");
-            newDiv.textContent = 'The computer has reached 2 points! You lose!';
+            newDiv.textContent = 'The computer has reached 5 points! You lose!';
             container.appendChild(newDiv);
+
             rockBtn.removeEventListener("click", chooseRock);
             paperBtn.removeEventListener("click", choosePaper);
             scissorsBtn.removeEventListener("click", chooseScissors);
         }else{
-            let newDiv = document.createElement("div");
-            let container = document.getElementById("buttons");
-            newDiv.textContent = ('You: ' + humanScore + ' Computer: ' + computerScore + '.');
-            container.appendChild(newDiv);
+            return;
         }
     }
     
